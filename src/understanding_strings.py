82cc711b8e62298ca2bd448e5a5bbad99d25f588
2530260 <2530260@upv.edu.mx>
2025-10-29 10:32:04 -0600

    feat

--- src/understanding_strings.py
+++ src/understanding_strings.py
@@ -16,3 +16,40 @@ Ejemplos:
  " El lenguaje 'python' lleva el nombre por Monty Python no por la serpiente "
 
 """
+
+name = "clase de programación"
+print(name)
+
+# title
+print(name.title())
+print(name)
+
+"""
+Un método es una acción que python 
+puede realizar en un fragmento de datos 
+o sobre una variable.
+
+     El punto . después de una variable 
+     seguido del metodo title() dice que 
+     se tiene que ejecutar el método title()
+     de la variable name.
+
+
+     Todos los métodos van seguidos de paréntesis 
+     porque en ocasiones nesecitan información adicional
+     para funcionar. la cual iría dentro de los paréntesis.
+     En esta ocasión, el método title() no requiere información 
+     adicional para funcionar.
+
+
+"""
+
+print("Método upper: ", name.upper())
+print("Método lower: ", name.lower())
+
+# Concatenación de STRINGS
+first_name = "jorge"
+last_name = "gonzález"
+full_name = first_name + " " + last_name
+print(full_name)
+print(full_name.title())
