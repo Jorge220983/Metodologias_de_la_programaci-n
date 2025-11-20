1eeee6255cb765fb03c2f4dc3677313fb8e08d40
2530260 <2530260@upv.edu.mx>
2025-11-20 10:33:34 -0600

    feat, src

---
+++ src/understanding_dictionary.py
@@ -0,0 +1,25 @@
+# Empty Dictionary
+homer_0 = {"color": 'yellow', 
+        "bag": 'maggie bag', 
+        "hair": 'black', "dress": 
+        'green', "mom": False,}
+print(homer_0)
+print(type(homer_0))
+
+marge = {"color": "yellow", 
+        'bag': "homer-donut", 
+        "hair": 'blue', "dress": 
+        'green', "mom": True,}
+
+gun_0 = {"scar": 'yellow-orange', 'headshot': 1.5}
+
+print(homer_0)
+homer_0["x-position"]=15
+homer_0["y-position"]=25
+homer_0["z-position"]=10
+print(homer_0)
+
+marge["x-position"]=16
+marge["y-position"]=26
+marge["z-position"]=10
+print(marge)
