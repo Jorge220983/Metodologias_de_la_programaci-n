d10d0d7ba2c7058f2a29df8d43a268002968e71d
2530260 <2530260@upv.edu.mx>
2025-11-24 10:40:12 -0600

    feat, src

--- src/understanding_dictionary.py
+++ src/understanding_dictionary.py
@@ -23,3 +23,38 @@ marge["x-position"]=16
 marge["y-position"]=26
 marge["z-position"]=10
 print(marge)
+
+alien_0 = {"color": 'yellow'}
+print(alien_0["color"])
+
+# Modifying an element of dictionary
+alien_0["color"] = "Blue"
+print(alien_0)
+
+# Adding elements to a dictionary
+alien_0["x_position"] = 0 
+alien_0["y_position"] = 25
+alien_0["name"] = "Paul"
+
+print(alien_0)
+
+# Looping though items
+print("\n Looping though items")
+for key, value in alien_0.items():
+        print(f"The key {key} has value {value}")
+
+# Looping though keys
+("\n Looping though keys")
+for key in alien_0.keys():
+        print(key)
+
+# Looping though values
+("\n Looping though value")
+for value in alien_0.values():
+        print(value)
+
+# NESTING 
+# Listas de diccionarios
+# Listas en diccionarios
+# Diccionarios en diccionarios
+
